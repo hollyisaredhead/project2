32bd83b285f1a2c27a25affaf05df9b34071933e
Holly Phelps <Hollyphelps1992@gmail.com>
2020-02-25 21:04:05 -0800

    Travis Test

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
  
 // Static directory
-app.use var(express.static("public"));
+app.use (express.static("public"));
 
 // Routes
 // =============================================================
@@ -23,7 +23,8 @@ require("./routes/")(app);
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    app.listen(PORT, function () {
+        console.log("App listening on PORT " + PORT);
+    
   });
 });
