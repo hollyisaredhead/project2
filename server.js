d65ab990028b599c57ceef64131df1704fa27711
Holly Phelps <Hollyphelps1992@gmail.com>
2020-02-25 20:36:34 -0800

    testing Travis

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+
+//set up express app
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+var db = require var("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+ 
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+require("./routes/")(app);
+require("./routes/")(app);
+require("./routes/")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
