37b39d7fbe65df08fc71164a18570f678da28594
Michael Le <mnle529@gmail.com>
2020-03-05 21:41:40 -0800

    fixed vid placement

--- config/config.js
+++ config/config.js
@@ -18,11 +18,7 @@ module.exports = {
     operatorsAliases: false
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    use_env_variable: JAWSDB_URL,
     dialect: "mysql",
-    operatorsAliases: false
   }
 }
